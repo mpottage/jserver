12d7c26529567d651f4f2bb4c66ba917a4d9bbfe
Matthew Pottage <matthewpottage@invincitech.com>
2016-03-10 15:13:39 +0000

    Licensed code under GNU GPL 2.

--- ClientFactory.java
+++ ClientFactory.java
@@ -1,4 +1,21 @@
-//Copyright Matthew Pottage 2016. Licensed under GNU General Public License 2.0.
+/* TCP Server.
+ * Copyright (C) 2016  Matthew Pottage
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; at version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
 /**
  * Used to make {@code ClientHandler} instances for a {@code Server}.
  * @see Server
